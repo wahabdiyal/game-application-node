5a8253439876d7d1887d237d86bfba6a566a0829
AppDevZoneOff <touqeer.appdevzone@gmail.com>
2023-07-26 15:34:43 +0500

    changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,62 +5,7 @@ import * as mongoose from 'mongoose';
 
 @Injectable()
 export class UserService {
-
-<<<<<<< HEAD
-  constructor(
-    @InjectModel(User.name)
-    private userModel: mongoose.Model<User>,
-  ) { }
-  async findAll(): Promise<User[]> {
-    const user = await this.userModel.find();
-    return user;
-  }
-  async create(user: User): Promise<User> {
-    return await this.userModel.create(user);
-    if (!user) {
-      const res = await this.userModel.create(user);
-      return res;
-    }
-    throw new BadRequestException('User already exists');
-  }
-  async findById(email: string): Promise<User> {
-
-    const user = await this.userModel.findOne({ email: email });
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
-    return user;
-  }
-
-  async update(id: any, body: any) {
-    const user = await this.userModel.findByIdAndUpdate(id, body);
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
-    return { status: true, message: "User updated successfully" };
-  }
-
-  async remove(id: any) {
-    const userdel = await this.userModel.findOneAndDelete(id);
-
-    return userdel;
-  }
-  async findwithUserId(id: any): Promise<User> {
-
-    const user = await this.userModel.findOne(id);
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
-    return user;
-  }
-=======
-    constructor(
+constructor(
         @InjectModel(User.name)
         private userModel: mongoose.Model<User>,
         ){}
@@ -120,5 +65,4 @@ export class UserService {
     
             return user;
           }
->>>>>>> a1186ff4f1d2928ffbaefd6a0060346838dafd0f
 }
