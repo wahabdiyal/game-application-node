f1541cb2fb7f3251cd280f6075691f81d6af906a
Asad-Ali-321 <asadnoorit@gmail.com>
2023-09-11 14:39:19 +0500

    user_bank and crypt_wallet

--- src/user_banks/schemas/user_banks.schema.ts
+++ src/user_banks/schemas/user_banks.schema.ts
@@ -2,16 +2,24 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from 'mongoose';
 @Schema({
-        timestamps: true,
+    timestamps: true,
 })
-export class UserBank  extends Document{
-     
-    @Prop({required: true})
-    bank_detail:[string];
-    @Prop({required: true})
-    user_id:string;
-    @Prop({default: "",})
-    remarks:string;
+export class UserBank extends Document {
+
+
+    @Prop({ required: true })
+    bank_name: string;
+    @Prop({ required: true })
+    account: string;
+    @Prop({ required: true })
+    iban: string;
+    @Prop({ required: true })
+    description: string;
+
+    @Prop({ required: true })
+    user_id: string;
+    // @Prop({ default: "", })
+    // remarks: string;
 }
 export const UserBankSchema = SchemaFactory.createForClass(UserBank)
 
