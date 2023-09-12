600fb0ac6b90ffd443dfbe2ea2d4ae413cf9edf9
Asad-Ali-321 <asadnoorit@gmail.com>
2023-09-12 14:05:21 +0500

    updates

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,53 +1,54 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from 'mongoose';
- 
-export enum UserType{
-    admin="admin",
-    manager="operator",
-    user="player"
+
+export enum UserType {
+    admin = "admin",
+    manager = "operator",
+    user = "player"
 }
 @Schema({
-        timestamps: true,
+    timestamps: true,
 })
-export class User extends Document{
-    
-    @Prop()
-    full_name:string;
-    @Prop()
-    first_name:string;
-    @Prop({default:null})
-    user_ip:string;
-    @Prop({default:0})
-    block:string;
-    @Prop()
-    last_name:string;
-    @Prop({required: true})
-    country:string;
-    @Prop({default: 'active'})
-    status:string;
+export class User extends Document {
 
-    @Prop({default: null,})
-    created_by:string;
-    
-    @Prop({default: null,})
-    updated_by:string;
+    @Prop()
+    full_name: string;
+    @Prop()
+    first_name: string;
+    @Prop({ default: null })
+    user_ip: string;
+    @Prop({ default: 0 })
+    block: string;
+    @Prop()
+    last_name: string;
+    @Prop({ required: true })
+    country: string;
+    @Prop({ default: 'active' })
+    status: string;
+
+    @Prop({ default: null, })
+    created_by: string;
+
+    @Prop({ default: null, })
+    updated_by: string;
 
     @Prop({ required: true, unique: true })
-    email:string;
+    email: string;
     @Prop()
-     password: string
-     
-     @Prop({default: "player"})
-    role:UserType;
-    @Prop({ required: true, unique: true })
-    phone:number;
+    password: string
 
-    @Prop({required: true,default:0})
-    silver_balance:string; 
-    @Prop({required: true,default:0})
-    gold_balance:string; 
-   @Prop({default:"image.png"})
-   file_url:string;
+    @Prop({ default: "player" })
+    role: UserType;
+
+    @Prop({ unique: true })
+    phone: string;
+
+    @Prop({ required: true, default: 0 })
+    silver_balance: string;
+    @Prop({ required: true, default: 0 })
+    gold_balance: string;
+    @Prop({ default: "image.png" })
+    file_url: string;
 }
 
 
