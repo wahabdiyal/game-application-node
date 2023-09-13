c0632035e98bb6e60ddea9efa89895377b4cc709
AppDevZoneOff <touqeer.appdevzone@gmail.com>
2023-09-13 18:49:19 +0500

    add middleware workign

--- src/auth/middlewares/auth.middleware.ts
+++ src/auth/middlewares/auth.middleware.ts
@@ -1,13 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { Mongoose } from 'mongoose';
-@Injectable()
- 
-export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly mongoose: Mongoose) {}
 
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const users = this.mongoose.model('users').find();
-    next(users);
+    console.log('Request...');
+    next();
   }
 }
