776f64e8f378cacd104c220777695963b1b70d6a
Asad-Ali-321 <asadnoorit@gmail.com>
2023-09-18 21:16:38 +0500

    updates

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,20 @@ import { Injectable, UnauthorizedException, NotAcceptableException } from '@nest
 import { UserService as UsersService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { AllowedIpsService } from 'src/allowed_ips/allowed_ips.service';
- 
+import mongoose from 'mongoose';
+import { User } from 'src/user/schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { AllowedIP } from 'src/allowed_ips/schemas/allowed_ips.schema';
+
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    @InjectModel(User.name)
+    private userModel: mongoose.Model<User>,
+    // @InjectModel(AllowedIP.name)
+    // private allowedIPService: mongoose.Model<AllowedIP>,
     private listIpService: AllowedIpsService
   ) { }
   async validateUser(user_id) {
@@ -51,10 +59,10 @@ export class AuthService {
       throw new NotAcceptableException("User is invalid, try to contact admin")
     }
     //&& user.role=="player" 
- 
-    if (user?.password !== pass  ) {
+
+    if (user?.password !== pass) {
       throw new NotAcceptableException("password is not valid");
- 
+
     }
     const payload = {
       id: user._id,
@@ -74,50 +82,68 @@ export class AuthService {
 
   async loginAdmin(email, pass, ip) {
 
-    const user = await this.usersService.findByEmail(email);
- 
+    let status = false;
+    let message = "";
+    let access_token = "";
 
-    /////handle attempts increments
-    if (!user) await this.usersService.update({ _id: user.id }, { $inc: { attempts: 1 } });
-    /////block user
-    if (user.attempts > 5) await this.usersService.update({ _id: user.id }, { status: 'blocked' });
- 
-    if(user.status !== 'active' ){
-      throw new NotAcceptableException("User Blocked, Not Allowed Access")
- 
+    let user = await this.userModel.findOne({ email: email, password: pass });
+
+    /////handle invalid,player & wrong attempts attempts increments
+    if (!user || user.role == "player") {
+      await this.userModel.updateOne({ email: email }, { $inc: { attempts: 1 } });
+      status = false; message = "invalid user"
     }
- 
+    /////handle block user & max attempts
+    else if (user.attempts > 5 || user.status !== 'active') {
+      if (user.attempts > 5)
+        await this.usersService.update({ _id: user.id }, { status: 'blocked' });
+      status = false; message = "user blocked, contact admin"; user = null
+    }
+    else {
+      ///check for allowed ip
+      const IPallowed = await this.listIpService.findUserIp(ip, user._id.toString());
+      if (IPallowed == null) { status = false; message = "ip not allowed, contact admin"; user = null }
+      ///proceed for success login
+      else {
+        let r = (Math.random() * 36 ** 16).toString(36);
+        await this.usersService.update({ _id: user.id }, { user_login_token: r });
+        const payload = {
+          id: user._id,
+          name: user.first_name + user.last_name,
+          country: user.country,
+          email: user.email,
+          status: user.status,
+          role: user.role,
+          user_login_token: r
+        };
+        access_token = await this.jwtService.signAsync(payload)
+        status = true; message = "success"
+      }
+    }
+
+    return {
+      status: status,
+      message: message,
+      user: user,
+      access_token: access_token,
+    };
+
     // const getIp  = await this.listIpService.findUserIp(ip,user.id);
-  
+
     // if (getIp['status'] == false) {
     //   throw new NotAcceptableException("Not Allowed Access")
     // }
     // if(user.user_ip==null){
     //   await this.usersService.update({_id:user.id},{user_ip:ip});
     // }
-    
-   
-    if (user?.password !== pass && user.role == "player") {
- 
-      throw new UnauthorizedException();
-    }
 
-    let r = (Math.random() * 36 ** 16).toString(36);
 
-    await this.usersService.update({ _id: user.id }, { user_login_token: r });
-    const payload = {
-      id: user._id,
-      name: user.first_name + user.last_name,
-      country: user.country,
-      email: user.email,
-      status: user.status,
-      role: user.role,
-      user_login_token: r
-    };
-    return {
-      status: true,
-      user: await this.usersService.findByEmail(email),
-      access_token: await this.jwtService.signAsync(payload),
-    };
+    // if (user?.password !== pass && user.role == "player") {
+
+    //   throw new UnauthorizedException();
+    // }
+
+
+
   }
 }
