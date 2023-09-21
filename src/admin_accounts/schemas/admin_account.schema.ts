70c4a906c2eaae1d8a4bf7fdb078ebd637f51530
AppDevZoneOff <touqeer.appdevzone@gmail.com>
2023-09-21 19:26:37 +0500

    error find in user controller and admin account schema

--- src/admin_accounts/schemas/admin_account.schema.ts
+++ src/admin_accounts/schemas/admin_account.schema.ts
@@ -15,19 +15,18 @@ export class AdminAccount  extends Document{
     @Prop({required: true,default:"0"})
     gold_coin_balance:string;
     @Prop()
-<<<<<<< HEAD
+ 
     client_id:string;
 
     @Prop()
     created_by: string;
     @Prop()
     updated_by: string;
-=======
+ 
     user_id:string;
     @Prop()
     email:string;
->>>>>>> e7526af7e8137267feda525e9b81d89e2f312a42
-    
+ 
 }
 export const AdminAccountSchema = SchemaFactory.createForClass(AdminAccount)
 
