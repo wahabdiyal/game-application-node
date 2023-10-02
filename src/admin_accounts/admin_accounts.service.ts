ffbc532eaa26bbb0ae40c3e7f0e2b0b27ece8538
Asad-Ali-321 <asadnoorit@gmail.com>
2023-10-02 16:53:49 +0500

    updates

--- src/admin_accounts/admin_accounts.service.ts
+++ src/admin_accounts/admin_accounts.service.ts
@@ -10,45 +10,45 @@ export class AdminAccountsService {
   constructor(
     @InjectModel(AdminAccount.name)
     private accountService: mongoose.Model<AdminAccount>
-  ){}
+  ) { }
   async create(createAdminAccountDto: CreateAdminAccountDto) {
     // return {...createAdminAccountDto,"gold_coin_balance":Number(createAdminAccountDto['gold_coin_balance'])+Number(createAdminAccountDto['credit'])}
-    var res = await this.accountService.create( createAdminAccountDto );
+    var res = await this.accountService.create(createAdminAccountDto);
     return res;
   }
 
- async findAll() {
-  return await this.accountService.find();
+  async findAll() {
+    return await this.accountService.find();
   }
 
- async findOne(id: any) {
-    return await this.accountService.findOne({_id : id});
+  async findOne(id: any) {
+    return await this.accountService.findOne({ _id: id });
   }
 
   async update(id: any, updateAdminAccountDto: UpdateAdminAccountDto) {
-    const account = await this.accountService.findByIdAndUpdate(id,updateAdminAccountDto);
+    const account = await this.accountService.findByIdAndUpdate(id, updateAdminAccountDto);
 
     if (!account) {
       throw new NotFoundException('account not found.');
     }
-  
-    return {status: true,message: "account updated successfully"};
+
+    return { status: true, data: await this.accountService.findOne({ _id: id }), message: "account updated successfully" };
   }
 
   async remove(id: any) {
     const account = await this.accountService.findByIdAndDelete(id);
 
-    if (!account){
+    if (!account) {
       throw new NotFoundException('account not found.');
     }
-  
-    return {status: true,message: "account Delete successfully"};
+
+    return { status: true, message: "account Delete successfully" };
   }
   async getLatestEntry(): Promise<any> {
     try {
       const latestRecord = await this.accountService
         .findOne()
-        .sort({ createdAt: -1 }) 
+        .sort({ createdAt: -1 })
         .exec();
       return latestRecord;
     } catch (error) {
