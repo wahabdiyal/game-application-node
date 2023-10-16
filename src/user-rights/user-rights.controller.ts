cb2b9c643e9e4908ab1805f28c056349fde1a594
Asad-Ali-321 <asadnoorit@gmail.com>
2023-10-16 10:59:49 +0500

    updated

--- src/user-rights/user-rights.controller.ts
+++ src/user-rights/user-rights.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UserRightsService } from './user-rights.service';
 import { CreateUserRightDto } from './dto/create-user-right.dto';
 import { UpdateUserRightDto } from './dto/update-user-right.dto';
@@ -23,8 +23,12 @@ export class UserRightsController {
   @Get('role/:id')
   findByRole(@Param('id') id: string) {
     return this.userRightsService.findByRole(id);
-  }
+  } 
   
+  @Get('find-access/roles')
+  findByRoleByController(@Query() { controller, role}) {
+    return this.userRightsService.findByRoleByController(controller,role);
+  }  
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserRightDto: UpdateUserRightDto) {
